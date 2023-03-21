4ec22d8e103b194e614d17ee692d715e545eddb9
arofficial005 <abdurrehman4009@gmail.com>
2023-03-21 22:58:29 +0500

    new

--- App.tsx
+++ App.tsx
@@ -5,8 +5,15 @@ import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Routes from './src/routes';
 import 'react-native-gesture-handler';
+import SplashScreen from "react-native-splash-screen";
+
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide(); //hides the splash screen on app load.
+  }, []);
+
 
   return (
         <SafeAreaProvider>
